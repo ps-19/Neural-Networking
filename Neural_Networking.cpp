ad9ac4540c495ae73f359d86b691755ee581cf66
Priyansh Singh <63330165+ps-19@users.noreply.github.com>
2021-04-08 21:51:47 +0530

    Update Neural_Networking.cpp

--- Neural_Networking.cpp
+++ Neural_Networking.cpp
@@ -1,4 +1,33 @@
+/**
+ *    author: ps_19
+**/
 #include <bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define ll  long long
+#define int long long
+#define ld  long double
+#define endl "\n"
+#define pb  push_back
+#define fill(a,val) memset(a,val,sizeof(a))
+#define ff  first
+#define ss  second
+#define test  ll t; cin>>t; while(t--)
+#define loop(i,a,b)  for(ll i=a;i<b;i++)
+#define loopr(i,a,b) for(ll i=a;i>=b;i--)
+#define pii pair<ll,ll>
+#define all(v) v.begin(),v.end()
+const ll mod     = 1000*1000*1000+7;
+const ll inf     = 1ll*1000*1000*1000*1000*1000*1000 + 7;
+const ll mod2    = 998244353;
+const ll N       = 1000 + 10;
+const ld pi      = 3.141592653589793;
+ll power(ll x,ll y,ll p = LLONG_MAX ){ll res=1;x%=p;while(y>0){if(y&1)res=(res*x)%p;y=y>>1;x=(x*x)%p;}return res;}
+ll ncr(ll n,ll r,ll m){if(r>n)return 0;ll a=1,b=1,i;for(i=0;i<r;i++){a=(a*n)%m;--n;}while(r){b=(b*r)%m;--r;}return (a*power(b,m-2,m))%m;}
+
+
                       // topology[layers] vector contains the number of layers in that particular layer.
                      // firstly we will assume that all layers of neuron are tightly connected to each other and their is only one layer which have any constant value called bias neuron.
 using namespace std;
